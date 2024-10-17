4012bbe7d656bb8f4ac425e7c71a004fa125ad5b
Saksham-Kamble <127680181+Saksham-Kamble@users.noreply.github.com>
2024-10-17 10:22:07 +0530

    Create install_package.sh

---
+++ install_package.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+#This script will install any package
+
+
+echo"*****************************************INSTALLING $1****************************************"
+sudo apt-get update
+sudo apt-get install $1 -y
+
+sudo systemctl start $1
+sudo systemctl enable $1
+echo"************************************************DONE**********************************************"
